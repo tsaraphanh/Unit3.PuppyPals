272a3d6237c45810c5a2d4e330a30719ab383798
Troy Saraphanh <troysaraphanh@Troys-MacBook-Air.local>
2024-01-29 20:59:48 -0600

    added select a featured puppy feature to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import { useState } from 'react'
 import { puppyList } from './data.js'
+import './index.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
+
   return (
     <div>
       {
        puppies.map((puppy) => {
-        return <p>{puppy.name}</p>
+        return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
        }) 
       }
+      { featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
